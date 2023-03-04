ca6981850c0eb6f2036f9bc1f07e5d1369390fbc
Nacho Alesina <ignacioalesina@gmail.com>
2023-03-04 12:08:19 -0300

    Vacio

---
+++ prueba.cpp
@@ -0,0 +1,19 @@
+
+  
+// Driver Code
+int main()
+{
+    // Variable declaration
+    int a, b;
+    float c;
+  
+    a = 20;
+    b = 40;
+  
+    // Typecasting
+    c = (float)a * b;
+  
+    cout << "Result: " << c;
+  
+    return 0;
+}
