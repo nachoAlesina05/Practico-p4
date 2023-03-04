3272c9641ace7c76d2608291781cc1eb96cf3c0f
nachoAlesina05 <ignacioalesina@gmail.com>
2023-03-04 13:57:52 -0300

    Pruebita

--- prueba.cpp
+++ prueba.cpp
@@ -1,19 +1,19 @@
+class pr
+{
+private:
+    float i,j;       
+public:
+    int funcion(int x);
+};
 
-  
-// Driver Code
+pr::funcion(int x)
+{
+    return x;
+}
 int main()
 {
-    // Variable declaration
-    int a, b;
-    float c;
-  
-    a = 20;
-    b = 40;
-  
-    // Typecasting
-    c = (float)a * b;
-  
-    cout << "Result: " << c;
-  
+    
     return 0;
 }
+
+
