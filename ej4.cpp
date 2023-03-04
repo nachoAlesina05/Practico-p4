5b2a42650888f1a57a11b2bc663a9cbcfe277bf7
nachoAlesina05 <108947529+nachoAlesina05@users.noreply.github.com>
2023-03-04 15:46:34 -0300

    Create ej4.cpp

---
+++ ej4.cpp
@@ -0,0 +1,16 @@
+
+
+class Punto{
+  private: 
+    int x,int y;
+  public:
+    Punto crearPunto();
+    Punto setPunto(Punto p, int x,int y);
+    int posX(Punto);
+    int posY(Punto);
+}
+
+
+void main(){
+  return 0
+}
