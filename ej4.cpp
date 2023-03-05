a13c6c97798d0fecf5b5ca39b74e794807c2fdf7
nachoAlesina05 <ignacioalesina@gmail.com>
2023-03-04 21:21:58 -0300

    Commit final, lei el instructivo, buena practica usando constructores y destructores.

--- ej4.cpp
+++ ej4.cpp
@@ -4,41 +4,35 @@ class Punto{
   private: 
     int x; int y;
   public:
-    Punto* crearPunto(); //Lo inicializa en 0
-    void setPunto(Punto *p, int x,int y);
-    void mostrarPunto(Punto *p);
-    int posX(Punto* p);
-    int posY(Punto* p);
+    Punto();
+    Punto(int,int);
+    Punto(const Punto &);
+    ~Punto();
+    void mostrarPunto();
 };
+  Punto::Punto(){
+    x=0;
+    y=0;
+  }
+  Punto::Punto(int x,int y){
+    (*this).x=x;
+    (*this).y=y;
+  };
+  void Punto::mostrarPunto(){
+    printf("El valor del punto es (%d,%d)", x ,y);
+  };
+  Punto::~Punto(){
 
-Punto* Punto::crearPunto(){
-  Punto* a = new Punto;
-  a->x=0;
-  a->y=0;
-  return a;
-}
-
-void Punto::mostrarPunto(Punto *p){
-  printf("X: %d", x);
-}
-
-void Punto::setPunto(Punto *p,int x, int y){
-  p->x=x;
-  p->y=y;
-}
-
-int Punto::posX(Punto* p){
-  return p->x;
-}
-int Punto::posY(Punto* p){
-  return p->y;
-}
-
-
+  }
 
 int main(){
-  Punto* p= p->crearPunto();
-  p->setPunto(p,3,4);
-  p->mostrarPunto(p);
+  Punto p;
+  p.mostrarPunto();
+  Punto *pp = new Punto(1,2);
+  pp->mostrarPunto();
+  Punto *ppp=pp;
+  ppp->mostrarPunto();
+  delete pp;  
+  delete ppp;
   return 0;
 }
