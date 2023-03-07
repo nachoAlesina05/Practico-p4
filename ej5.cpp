3d1f51d3974d2f55e486f17aeec4127a7e08997b
NachoAlesina <ignacioalesina@gmail.com>
2023-03-07 12:04:44 -0300

    Ejercicio 5 finalizado lo hecho con exito, no quedaron todas las funciones pedidas finalizadas

--- ej5.cpp
+++ ej5.cpp
@@ -1,4 +1,4 @@
-//#include <iostream>
+#include <iostream>
 
 class Fecha{
     private:
@@ -21,11 +21,16 @@ class Fecha{
         m=10;
         a=2001;
     }
-    Fecha::Fecha(int dia,int mes,int anio){
-        d=dia;
-        a=anio;
-        m=mes;
+    Fecha::Fecha(int dia, int mes, int anio){
+    d = dia;
+    m = mes;
+    a = anio;
     }
+    Fecha::Fecha(const Fecha& f){
+    d = f.d;
+    m = f.m;
+    a = f.a;
+}
     Fecha::~Fecha(){};
 
     Fecha Fecha:: avanzar(int cant){ //Supondremos todos los meses tienen 30 dias
@@ -39,5 +44,21 @@ class Fecha{
             f.d = cant;
         }    
         return f;            
+    }
+
+    bool Fecha::comparacion(Fecha f){
+        return (f.a == a && f.m == m && f.d == d);
+    }
+
+    int main(){
+        Fecha f1;
+        Fecha f2(25,10,2001);
+        if(!f1.comparacion(f2))
+            std::cout << "Las fechas son distintas \n";
+        f2 = f2.avanzar(1);
+        if(f1.comparacion(f2))
+            std::cout << "Las fechas son iguales \n";
+        std::cout << "ChauMundo \n";
+        return 0;
     }
 
